fa7b518f80af1a29380191f632fe20db60afba04
emad-sfc <emad.sfc@gmail.com>
2021-12-14 21:36:01 +0530

    fibonacci

---
+++ DSA-Algo/src/Recursion/Fibonacci/execFibonacci.java
@@ -0,0 +1,12 @@
+package Recursion.Fibonacci;
+
+public class execFibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		fibonacci obj = new fibonacci();
+		int sum = obj.fib(6);
+		System.out.println(sum);
+	}
+
+}
